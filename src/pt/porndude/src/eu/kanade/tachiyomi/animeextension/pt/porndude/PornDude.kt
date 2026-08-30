0fdc26c2526a920f2bc7f916f832f02f8b0e6117
julia5454554 <julia5454554@gmail.com>
2026-08-30 11:06:52 -0300

    Update PornDude.kt

--- src/pt/porndude/src/eu/kanade/tachiyomi/animeextension/pt/porndude/PornDude.kt
+++ src/pt/porndude/src/eu/kanade/tachiyomi/animeextension/pt/porndude/PornDude.kt
@@ -1,11 +1,11 @@
 package eu.kanade.tachiyomi.animeextension.pt.porndude
 
-import eu.kanade.tachiyomi.animesource.online.AnimeHttpSource
 import eu.kanade.tachiyomi.animesource.model.AnimeFilterList
 import eu.kanade.tachiyomi.animesource.model.AnimesPage
 import eu.kanade.tachiyomi.animesource.model.SAnime
 import eu.kanade.tachiyomi.animesource.model.SEpisode
 import eu.kanade.tachiyomi.animesource.model.Video
+import eu.kanade.tachiyomi.animesource.online.AnimeHttpSource
 import eu.kanade.tachiyomi.network.GET
 import eu.kanade.tachiyomi.util.asJsoup
 import okhttp3.Request
