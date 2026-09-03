ea5a975cbb335e76e0c004867ea2ab7ed0f67a7c
julia5454554 <julia5454554@gmail.com>
2026-09-03 20:14:30 -0300

    Update MegaHentai.kt

--- src/pt/megahentai/src/eu/kanade/tachiyomi/animeextension/pt/megahentai/MegaHentai.kt
+++ src/pt/megahentai/src/eu/kanade/tachiyomi/animeextension/pt/megahentai/MegaHentai.kt
@@ -273,7 +273,8 @@ class MegaHentai :
 
     // ============================= Utilities ==============================
     private val bloggerExtractor by lazy { BloggerExtractor(client) }
-    private val universalExtractor by lazy { UniversalExtractor(client) }
+    // Correção: passar o context para o UniversalExtractor
+    private val universalExtractor by lazy { UniversalExtractor(client, context) }
 
     companion object {
         private val EPISODE_NUMBER_REGEX = "(?:Epis[oó]dio\\s+)([0-9]+(?:\\.[0-9]+)?)".toRegex(RegexOption.IGNORE_CASE)
